99cfc6f4d151c9ba965409bb1c809f3698a56679
NTCG25 <65662881+NTCG25@users.noreply.github.com>
2021-09-08 17:13:27 -0500

    Add files via upload

---
+++ Algebra.py
@@ -0,0 +1,12 @@
+num_1 = 12
+num_2 = 99
+num_3 = 20
+
+user = input("What is 12 + 99 - 20?: ")
+
+
+correct_answer = 91
+
+if user == correct_answer:
+    print("Well done, Genius")
+
---
+++ Multiplication.py
@@ -0,0 +1,4 @@
+num = 10
+
+for i in range (1, 16):
+    print(num, "x", i, '=', num*i)
