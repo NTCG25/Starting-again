b5cbb3035f51eb2264f046e16e71807e889fb9ff
NTCG25 <65662881+NTCG25@users.noreply.github.com>
2021-09-09 17:04:42 -0500

    Add files via upload

--- API Testing.py
+++ API Testing.py
@@ -1,8 +1,13 @@
-from flask import Flask, request
-from flask_restful import Api, Resource
+from flask import Flask
+from flask_restful import Api, Resource,reqparse
 
 app = Flask(__name__)
 api = Api(app)
+
+video_put_args = reqparse.RequestParser()
+video_put_args.add_argument("name",type=str, help="Name of the video is required", required=True)
+video_put_args.add_argument("views",type=int, help="Views of the video", required=True)
+video_put_args.add_argument("likes",type=int, help="Likes on the video")
 
 videos = {}
 
@@ -10,9 +15,10 @@ class Video(Resource):
     def get(self, video_id):
         return videos[video_id]
 
-def put(self, video_id):
-    print(request.form)
-    return {}
+    def put(self, video_id):
+        args = video_put_args.parse_args()
+        videos[video_id] = args
+        return videos[video_id], 201
 
 api.add_resource(Video, "/video/<int:video_id>")
 
--- Test of API.py
+++ Test of API.py
@@ -2,5 +2,8 @@ import requests
 
 BASE ="http://127.0.0.1:5000/"
 
-response = requests.get(BASE + "video/1", {"likes": 16})
+response = requests.put(BASE + "video/1", {"likes": 440, "name": "NTCG25", "views": "956"})
+print(response.json())
+input()
+response = requests.get(BASE + "video/1")
 print(response.json())
