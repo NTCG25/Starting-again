9c92fa6647f8a27c93d45a06a61462d991969d77
NTCG25 <65662881+NTCG25@users.noreply.github.com>
2021-09-21 10:42:43 -0500

    Add files via upload

--- API Testing.py
+++ API Testing.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask_restful import Api, Resource,reqparse
+from flask_restful import Api, Resource,reqparse, abort
 
 app = Flask(__name__)
 api = Api(app)
@@ -11,14 +11,28 @@ video_put_args.add_argument("likes",type=int, help="Likes on the video")
 
 videos = {}
 
+def abort_if_video_doesnt_exits(video_id):
+    if video_id not in videos:
+        abort(404, message="That video could not be found...")
 class Video(Resource):
     def get(self, video_id):
+        abort_if_video_doesnt_exits(video_id)
         return videos[video_id]
 
     def put(self, video_id):
+        abort_if_video_exist(video_id)
         args = video_put_args.parse_args()
         videos[video_id] = args
         return videos[video_id], 201
+
+def abort_if_video_exist(video_id):
+    if video_id in videos:
+        abort(409, message="That video already exists...")
+
+ def delete(self, video_id):       # IndentationError, unsolved, line 32
+     abort_if_video_doesnt_exits(video_id)
+     del videos[video_id]
+     return '', 204
 
 api.add_resource(Video, "/video/<int:video_id>")
 
--- Test of API.py
+++ Test of API.py
@@ -2,8 +2,8 @@ import requests
 
 BASE ="http://127.0.0.1:5000/"
 
-response = requests.put(BASE + "video/1", {"likes": 440, "name": "NTCG25", "views": "956"})
-print(response.json())
-input()
-response = requests.get(BASE + "video/1")
+#response = requests.put(BASE + "video/1", {"likes": 440, "name": "NTCG25", "views": "956"})
+#print(response.json())
+#input()
+response = requests.get(BASE + "video/6")
 print(response.json())
