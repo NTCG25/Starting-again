fad7efe5c529581a78828a941a04e8eec7d2f629
NTCG25 <65662881+NTCG25@users.noreply.github.com>
2021-10-28 09:30:20 -0500

    Add files via upload

---
+++ Static and Class Methods.py
@@ -0,0 +1,20 @@
+class person(object):
+    population = 95
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    @classmethod
+    def getPopulation(cls):
+        return cls.population
+
+    @staticmethod
+    def isAdult(age):
+        return age >= 18
+
+    def display(self):
+        print(self.name, 'is', self.age, 'years old')
+
+newPerson = person("Nigel", "18")
+
+print(person.isAdult(18))
