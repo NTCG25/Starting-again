7046466c1244750659156ceafab52a5d7c744c57
NTCG25 <65662881+NTCG25@users.noreply.github.com>
2021-11-18 10:17:35 -0600

    Add files via upload

---
+++ My Cat.py
@@ -0,0 +1,13 @@
+class Cat:
+    scientificName = "Felis Catus"
+    petName = "Andrew"
+
+    def cats(self):
+        print("I'm part of the", self.scientificName)
+        print("My Name is", self.petName)
+
+        Andrew = Cat()
+
+        print(Andrew.scientificName)
+        Andrew.cats()
+
