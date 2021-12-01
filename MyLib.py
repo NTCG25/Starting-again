a81a1911574866a5ecc4d8305922839fd70a8b75
NTCG25 <65662881+NTCG25@users.noreply.github.com>
2021-12-01 10:52:38 -0600

    Add files via upload

---
+++ MyLib.py
@@ -0,0 +1,6 @@
+Noun = input("Noun: ")
+verb1 = input("Verb: ")
+
+madlib = f"I was walking home when I saw a cute {Noun}. It was so cute that I decided to take it home. When it got home it {verb1} on the carpet."
+
+print(madlib)
